3d0e5f9034b39fb60f27120a1d1cf5143633e149
jinyang_kim <sunlover011@gmail.com>
2024-12-21 04:04:42 +0900

    feat: 8.2.4 범위 슬롯 (date.24.12.20)
    - slot 학습
    - slot 기본 학습
    - 명명된 slot 학습
    - 범위 slot 학습

---
+++ slot-test/src/App2.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout>
+    <template v-slot:header>
+      <h2>헤더 영역</h2>
+    </template>
+    <template v-slot:sidebar>
+      <h3>사이드</h3>
+      <h3>사이드</h3>
+      <h3>사이드</h3>
+    </template>
+    <template v-slot:default>
+      <h1>컨텐츠 영역</h1><h2>콘텐츠 영역</h2><h3>콘텐츠 영역</h3>
+      <h1>컨텐츠 영역</h1><h2>콘텐츠 영역</h2><h3>콘텐츠 영역</h3>
+    </template>
+    <template v-slot:footer>
+      <h2>Footer text</h2>
+    </template>
+  </Layout>
+</template>
+
+<script>
+import Layout from '@/components/Layout.vue';
+
+export default {
+  name: "App2",
+  components: {Layout}
+};
+</script>
+
+<style scoped>
+
+</style>
